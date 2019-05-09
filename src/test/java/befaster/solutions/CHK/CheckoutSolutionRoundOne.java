c1a3498689ec682a1399e8f05114f69722c62aaa
sbx_user1051 <sbx_user1051@169.254.9.85>
2019-05-09 10:42:46 +0000

    Thu May 09 10:42:46 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionRoundOne.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionRoundOne.java
@@ -5,6 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/*
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+
+*/
+
 public class CheckoutSolutionRoundOne {
 	private CheckoutSolution chkSol;
 
@@ -15,7 +29,10 @@ public class CheckoutSolutionRoundOne {
 	}
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		assertThat(chkSol.checkout("4A,3B,C,D"), equalTo(300));
+		
 	}
 
 }
+
