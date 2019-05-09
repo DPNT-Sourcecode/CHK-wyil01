a949818d244c47cf7d198c1236ada92a2dc04771
sbx_user1051 <sbx_user1051@169.254.9.85>
2019-05-09 10:33:45 +0000

    Thu May 09 10:33:45 UTC 2019

---
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionRoundOne.java
@@ -0,0 +1,20 @@
+package befaster.solutions.CHK;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CheckoutSolutionRoundOne {
+	private CheckoutSolution chkSol;
+
+	@Before 
+	public void setUp()
+	{
+		chkSol = new CheckoutSolution();
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
